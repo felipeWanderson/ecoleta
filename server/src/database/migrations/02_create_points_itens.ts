1cd55c1318e12d940bb28fc2425ac33d006b1714
Felipe Leal <felipewanderson2020@gmail.com>
2020-06-03 01:48:45 -0300

    banco de dados e criação dos pontos de coletas

---
+++ server/src/database/migrations/02_create_points_itens.ts
@@ -0,0 +1,14 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('points_items', (table) => {
+    table.increments('id').primary();
+
+    table.string('point_id').notNullable().references('id').inTable('points');
+    table.string('item_id').notNullable().references('id').inTable('items');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('points_items');
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(process.env.PORT, () => {
   console.log(`Serividor rodando na porta::${process.env.PORT}`);

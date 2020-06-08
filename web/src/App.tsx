77d247572e8180b5d37132efb4ab9296d2e0db6a
Felipe Leal <felipewanderson2020@gmail.com>
2020-06-08 01:53:58 -0300

    frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import './App.css';
+
+import Routes from './router';
 
 function App() {
-  return <h1>Hello Ecoleta</h1>;
+  return <Routes />;
 }
 
 export default App;
